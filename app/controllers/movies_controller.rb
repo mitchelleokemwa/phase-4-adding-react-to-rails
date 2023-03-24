25f42debeb8e38a3bdc0529a43a4cacd4e7c313f
git_mitchelleokemwa <mitchelleokemwa@gmail.com>
2023-03-24 07:30:10 +0300

    Completed Assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,19 @@
-class MoviesController < ApplicationController
-  def index
-    movies = Movie.all
-    render json: movies
-  end
+class MoviesController < ApplicationController 
+   
+  def index 
+    movies = Movie.all 
+    render json: movies 
+  end 
+ 
+  def create  
+    movie = Movie.create(movie_params) 
+    render json: movie  
+  end  
+ 
+  private  
+
+  def movie_params 
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end 
+
 end
